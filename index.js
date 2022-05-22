babe09b7ce77f559aa8c07b0fc85f17bc1b6fafb
alhabib911 <abdullahalhabib100@gmail.com>
2022-05-22 15:15:21 +0600

    products data get

---
+++ index.js
@@ -0,0 +1,46 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors');
+require ('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qypq1.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect()
+        const productCollection =client.db('kelong').collection('products')
+
+
+        app.get('/product', async(req, res) =>{
+            const query = {}
+            const cursor = productCollection.find(query)
+            const product = await cursor.toArray()
+            res.send(product)
+        })
+    }
+    finally{
+
+    }
+}
+ 
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello From Kelong Group!')
+})
+
+app.listen(port, () => {
+  console.log(`Kelong app listening on port ${port}`)
+})
